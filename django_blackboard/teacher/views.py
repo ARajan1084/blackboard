113dc6e1b70bd507d701bd26ac7463611be3bf33
Achintya Rajan <39139575+ARajan1084@users.noreply.github.com>
2020-04-18 13:34:37 -0700

    gradesheet support!

--- django_blackboard/teacher/views.py
+++ django_blackboard/teacher/views.py
@@ -1,4 +1,5 @@
 import uuid
+from collections import OrderedDict
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
@@ -6,22 +7,22 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserLoginForm
 from .models import Teacher
 from board.models import Class, ClassAssignments, Course, Assignment
-from student.models import ClassEnrollment, Student
+from student.models import ClassEnrollment, Student, Submission
 
 
 @login_required(login_url='teacher-login')
 def home(request):
     teacher = Teacher.objects.all().get(user=request.user)
-    classes = Class.objects.all().filter(teacher_id=teacher.teacher_id)
+    classes = Class.objects.all().filter(teacher_id=str(teacher.id.hex))
     class_data = []
     for klass in classes:
         course = Course.objects.all().get(course_id=klass.course_id)
-        num_students = ClassEnrollment.objects.all().filter(class_id=klass.class_id).count()
+        num_students = ClassEnrollment.objects.all().filter(class_id=str(klass.id.hex)).count()
         class_data.append(
             [klass.period,
              course.course_name,
              num_students,
-             klass.class_id]
+             str(klass.id.hex)]
         )
     context = {
         'class_data': class_data
@@ -31,31 +32,38 @@ def home(request):
 
 @login_required(login_url='teacher-login')
 def gradesheet(request, class_id):
-    student_names = get_student_names(class_id)
-    assignments = get_assignments(class_id)
-    context = {
-        'student_names': student_names,
-        'assignments': assignments
-    }
-    return render(request, 'teacher/gradesheet.html', context)
-
-
-def get_student_names(class_id):
+    klass = Class.objects.all().get(id=uuid.UUID(class_id).hex)
+    course_name = Course.objects.all().get(course_id=klass.course_id).course_name
+    period = klass.period;
     enrollments = ClassEnrollment.objects.all().filter(class_id=class_id)
-    student_names = []
-    for enrollment in enrollments:
-        student = Student.objects.all().get(student_id=enrollment.student_id)
-        student_name = student.first_name + ' ' + student.last_name
-        student_names.append(student_name)
-    return student_names
-
-
-def get_assignments(class_id):
     assignment_ids = ClassAssignments.objects.all().filter(class_id=class_id)
     assignments = []
     for assignment_id in assignment_ids:
         assignments.append(Assignment.objects.all().get(id=uuid.UUID(assignment_id.assignment_id).hex))
-    return assignments
+    data = OrderedDict({})
+    for enrollment in enrollments:
+        student_name = get_student_name(enrollment)
+        for assignment in assignments:
+            submission = Submission.objects.get(enrollment_id=str(enrollment.id.hex), assignment_id=str(assignment.id.hex))
+            try:
+                current = data[student_name]
+                score = (assignment, submission)
+                data[student_name] = current + (score,)
+            except:
+                data.update({student_name: ((assignment, submission),)})
+    context = {
+        'course_name': course_name,
+        'period': period,
+        'assignments': assignments,
+        'data': data
+    }
+    return render(request, 'teacher/gradesheet.html', context)
+
+
+def get_student_name(enrollment):
+    student = Student.objects.all().get(student_id=enrollment.student_id)
+    student_name = student.first_name + ' ' + student.last_name
+    return student_name
 
 
 def login(request):
